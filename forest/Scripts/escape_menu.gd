3e45c86229a8377c1d67e265f6f19a055ef3f3ef
richardusakarickerd <richardwentzel52@gmail.com>
2025-04-01 21:10:39 +0200

    kniebis text

--- forest/Scripts/escape_menu.gd
+++ forest/Scripts/escape_menu.gd
@@ -1,5 +1,14 @@
 extends Control
 
+var is_paused = false
+
+#func _input(event):
+	#if event.is_action_pressed("Map"): 
+		#toggle_pause()
+
+#func toggle_pause():
+	#is_paused = !is_paused
+	#get_tree().paused = is_paused  # Zet de game op pauze
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
